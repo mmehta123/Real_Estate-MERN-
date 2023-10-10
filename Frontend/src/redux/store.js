f7a89a5f5bc5837e6862ef8d5f89a246f3129496
mmehta123 <mohitmehta285@gmail.com>
2023-10-10 10:32:00 +0530

    state persistor

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,12 +1,26 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
-import userSlice from "./user/userSlice";
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import userReducer from "./user/userSlice";
+import storage from "redux-persist/lib/storage";
+
+const persistConfigs = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const rootReducer = combineReducers({ user: userReducer });
+const persistedReducer = persistReducer(persistConfigs, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userSlice,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+export const persistor = persistStore(store);
